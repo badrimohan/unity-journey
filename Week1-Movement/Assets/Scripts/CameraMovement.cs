fd83d8a7cb230346a9ff858e5b5929ad18def363
Rajesh Mohan <rajmohr@Rajeshs-MBP.home>
2020-03-28 14:05:34 -0400

    Added Week 1 files

---
+++ Week1-Movement/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject character;
+    [SerializeField]
+    private Vector3 distanceFromCharacter;
+    private Vector3 prevCharacterPosition;
+    private Vector3 currCharacterPosition;
+    private Vector3 positionDelta;
+    private float rotateHorizontal;
+    private float rotateVertical;
+    [SerializeField]
+    private float Sensitivity = 2.0f;
+    private Vector3 prevDistanceFromCharacter;
+    [SerializeField]
+    private float maxVRotationAngle = 45.0f;
+    [SerializeField]
+    private float minVRotationAngle = 0.0f;
+    private float vRotateAmount;
+    private float inputRotationAngle;
+    private float vRotationThreshold = 90.0f;
+    private float currentRotationX;
+    private Vector3 currCameraPosition;
+    private Vector3 targetCameraPosition;
+    [SerializeField]
+    private float smoothFactor = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    
+    private void Start() {
+        prevCharacterPosition = character.transform.position;
+        // UpdateCamDistance(distanceFromCharacter, prevCharacterPosition);
+        // gameObject.transform.position = prevCharacterPosition - distanceFromCharacter;
+        prevDistanceFromCharacter = distanceFromCharacter;
+
+        // Keep minimum vertical rotation positive.
+        if (minVRotationAngle < 0) {
+            minVRotationAngle = 0;
+        }
+    }
+
+    // Update the camera position based on the amount the character has moved.
+    Vector3 UpdateCamPosition(Vector3 prevCharacterPosition) {
+
+        // Obtain current position of the character.
+        currCharacterPosition = character.transform.position;
+
+        // Calculate the difference between camera's current position 
+        // and the character's position
+        positionDelta = currCharacterPosition - prevCharacterPosition;
+
+        // Move the camera by the same amount the character has moved.
+        // gameObject.transform.Translate(positionDelta, Space.World);
+        
+        //Experimental
+        currCameraPosition = gameObject.transform.position;
+        targetCameraPosition = currCameraPosition + positionDelta;
+        // gameObject.transform.position = Vector3.SmoothDamp(currCameraPosition, targetCameraPosition, ref velocity, smoothTime);
+        gameObject.transform.position = Vector3.Lerp(currCameraPosition, targetCameraPosition, Time.deltaTime * smoothFactor);
+
+        return currCharacterPosition;
+    }
+
+    void UpdateCamDistanceFromCharacter() {
+        if (distanceFromCharacter != prevDistanceFromCharacter) {
+            positionDelta += distanceFromCharacter - prevDistanceFromCharacter;
+            prevDistanceFromCharacter = distanceFromCharacter;
+            gameObject.transform.Translate(positionDelta, Space.Self);
+        }
+    }
+
+    // Rotates the camera around character's current position based on axis input.
+    // PS4 - Right Analog Stick
+    void UpdateCamRotation(Vector3 characterPosition) {
+        
+        // Obtain Axes input.
+        rotateHorizontal = Input.GetAxis("RightStickX");
+        rotateVertical = Input.GetAxis("RightStickY");
+
+        // Rotate camera about the character's current position based on horizontal input.
+        // gameObject.transform.RotateAround(characterPosition, Vector3.up, rotateHorizontal * Sensitivity);
+        transform.position = MyRotateAround(transform.position, characterPosition, Vector3.up, rotateHorizontal * Sensitivity);
+        // transform.LookAt(characterPosition, Vector3.up);
+
+        // Limit the rotation on the vertical input based on minVRotationAngle and maxVRotationAngle
+        // So, if the values were 0 and 45, the camera only rotates wihin those angles.
+        currentRotationX = gameObject.transform.rotation.eulerAngles.x;
+        inputRotationAngle = rotateVertical * Sensitivity + currentRotationX;
+        if (inputRotationAngle > maxVRotationAngle && inputRotationAngle < vRotationThreshold) {
+            vRotateAmount = maxVRotationAngle - currentRotationX;
+        } else if (minVRotationAngle >= 0 && inputRotationAngle < minVRotationAngle) {
+            vRotateAmount = minVRotationAngle - currentRotationX;
+        } else {
+            vRotateAmount = rotateVertical * Sensitivity;
+        }
+        gameObject.transform.RotateAround(characterPosition, transform.right, vRotateAmount);
+
+    }
+
+    Vector3 MyRotateAround(Vector3 position, Vector3 point, Vector3 axis, float angle) {
+        if (angle == 0) {
+            return position;
+        }
+        Vector3 result;
+        float distance = Vector3.Distance(position, point);
+        float yDistance = position.y - point.y;
+        float radius = Mathf.Sqrt(Mathf.Pow(distance, 2) - Mathf.Pow(yDistance, 2));
+
+        result.x = position.x + radius * Mathf.Cos(Mathf.Rad2Deg * angle);
+        result.y = 0;
+        result.z = position.z + radius * Mathf.Sin(Mathf.Rad2Deg * angle);
+
+        return result;
+    }
+    void Update()
+    {
+        // Update Camera Position
+        currCharacterPosition = UpdateCamPosition(prevCharacterPosition);
+        // Store current frame's character position
+        prevCharacterPosition = currCharacterPosition;
+        // Update camera position if changes are made to distance.
+        UpdateCamDistanceFromCharacter();
+    }
+    void LateUpdate() {
+        UpdateCamRotation(currCharacterPosition);
+    }
+}
---
+++ Week1-Movement/Assets/Scripts/movementController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    // public GameObject character;
+    public float moveSpeed = 50;
+    [SerializeField]
+    private float jumpVelocity = 4;
+    private Rigidbody rb;
+    private float pitch = 0.0f;
+    private float yaw = 0.0f;
+    public float rotateSpeed = 10;
+    public Transform cam;
+
+    private void Awake() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        // cam = gameObject.transform.parent.GetChild(0);
+    }
+    private void Update() {
+        // pitch -= Input.GetAxis("Mouse X");
+        // yaw += Input.GetAxis("Mouse X");
+        pitch = Input.GetAxis("Mouse Y");
+        yaw = Input.GetAxis("Mouse X");
+        rb.transform.Rotate(new Vector3(0.0f, yaw, 0.0f) * rotateSpeed, Space.World);
+    }
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        
+
+        // gameObject.transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime);
+        //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+        // Debug.Log((pitch, yaw));
+        rb.AddForce(cam.forward * moveSpeed * verticalInput, ForceMode.Impulse);
+        rb.AddForce(cam.right * moveSpeed * horizontalInput, ForceMode.Impulse);
+        // rb.AddForce(new Vector3(horizontalInput,0,verticalInput) * moveSpeed, ForceMode.Impulse);
+        // Debug.Log(cam);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+
+    }
+    // private void FixedUpdate() {
+    //      if (Input.GetButtonDown("Jump"))
+    //     {
+    //         // IEnumerator jumpCoroutine = jumpAction(2.0f);
+    //         // StartCoroutine(jumpCoroutine);
+    //         Debug.Log("Entered jump");
+    //         rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+    //     }
+        
+    // }
+
+    // private IEnumerator jumpAction(float waitTime)
+    // {
+    //     character.transform.Translate(new Vector3(0, jumpHeight, 0) * moveSpeed * Time.deltaTime);
+    //     yield return new WaitForSeconds(waitTime);
+    // }
+}
